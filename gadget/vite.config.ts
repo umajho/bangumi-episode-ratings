badc55c610445a8e63af5062f32f6044ed29b403
umajho <umajho@proton.me>
2024-08-25 04:19:25 +0800

    build(gadget): 构建为 userscript

--- gadget/vite.config.ts
+++ gadget/vite.config.ts
@@ -1,15 +1,27 @@
+import PackageJSON from "./package.json";
+
 import { resolve } from "node:path";
+
 import { defineConfig } from "vite";
+import Userscript from "vite-userscript-plugin";
 
 export default defineConfig({
-  plugins: [],
-  build: {
-    lib: {
+  plugins: [
+    Userscript({
       entry: resolve(__dirname, "src/main.ts"),
-      name: "BangumiEpisodeRatingsGadget",
-      fileName: "dist",
-      formats: ["iife"],
-    },
-    minify: false,
-  },
+      header: {
+        name: "bangumi-episode-ratings-gadget",
+        version: PackageJSON.version,
+        description: PackageJSON.description,
+        match: [
+          "https://bangumi.tv/*",
+          "https://bgm.tv/*",
+          "https://chii.in/*",
+        ],
+      },
+      esbuildTransformOptions: {
+        minify: false,
+      },
+    }),
+  ],
 });
