b3296c2e9360505a2655c4662f625a97add714cf
umajho <umajho@proton.me>
2024-08-25 03:52:40 +0800

    fix(gadget): typos

--- gadget/vite.config.ts
+++ gadget/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
-      name: "BangumiEpisodeRantingsGadget",
+      name: "BangumiEpisodeRatingsGadget",
       fileName: "dist",
       formats: ["iife"],
     },
