8e1a4a5e4e60fa6ce3d955254a37d597d37f6584
umajho <umajho@proton.me>
2024-09-05 18:05:42 +0800

    feat(gadget): 允许通过设置 localStorage 来切换 debug 用的 API entrypoint

--- gadget/src/env.ts
+++ gadget/src/env.ts
@@ -3,9 +3,15 @@ const env = {
     return import.meta.env.VITE_APP_AUTH_ENTRYPOINT_URL;
   },
   get APP_API_ENTRYPOINT(): string {
+    const debugURL = localStorage
+      .getItem(this.LOCAL_STORAGE_KEY_DEBUG_API_ENTRYPOINT_URL);
+    if (debugURL) return debugURL;
+
     return import.meta.env.VITE_APP_API_ENTRYPOINT_URL;
   },
 
+  LOCAL_STORAGE_KEY_DEBUG_API_ENTRYPOINT_URL:
+    "bgm_ep_ratings_debug_api_entrypoint_url",
   LOCAL_STORAGE_KEY_TOKEN: "bgm_ep_ratings_token",
   LOCAL_STORAGE_KEY_JWT: "bgm_ep_ratings_jwt",
   SEARCH_PARAMS_KEY_TOKEN_COUPON: "bgm_ep_ratings_token_coupon",
