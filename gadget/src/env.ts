ad83cffa16263fd1e3232502bf1694eb5dab5062
umajho <umajho@proton.me>
2024-10-02 01:19:43 +0800

    build(gadget): 用 rolldown 替代 vite（lib mode）

--- gadget/src/env.ts
+++ gadget/src/env.ts
@@ -1,13 +1,13 @@
 const env = {
   get APP_AUTH_ENTRYPOINT(): string {
-    return import.meta.env.VITE_APP_AUTH_ENTRYPOINT_URL;
+    return process.env.APP_AUTH_ENTRYPOINT_URL!;
   },
   get APP_API_ENTRYPOINT(): string {
     const debugURL = localStorage
       .getItem(this.LOCAL_STORAGE_KEY_DEBUG_API_ENTRYPOINT_URL);
     if (debugURL) return debugURL;
 
-    return import.meta.env.VITE_APP_API_ENTRYPOINT_URL;
+    return process.env.APP_API_ENTRYPOINT_URL!;
   },
 
   LOCAL_STORAGE_KEY_DEBUG_API_ENTRYPOINT_URL:
